5f0a11c476c6c192f21b0deeb8f615ffd6bf99cd
SzokolAttila <szokol.ati2005@gmail.com>
2023-11-29 12:37:43 +0100

    feature class added

---
+++ assets/js/classes/features.mjs
@@ -0,0 +1,14 @@
+class Feature{
+    #id;
+    #name;
+    constructor({id, name}){
+        this.#id = id;
+        this.#name = name;
+    }
+    get id(){
+        return this.#id;
+    }
+    get name(){
+        return this.#name;
+    }
+}
