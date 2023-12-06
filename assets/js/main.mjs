0fd9069309c1f1ff4158b350355151dff56e6a98
SzokolAttila <szokol.ati2005@gmail.com>
2023-12-06 11:26:25 +0100

    features, hotels and offers arrays created

--- assets/js/main.mjs
+++ assets/js/main.mjs
@@ -1,1 +1,9 @@
 "use strict";
+
+import data from "./data/data.mjs";
+
+const features = data.features.map(x => x);
+const hotels = data.hotels.map(x => x);
+const offers = data.offers.map(x => x);
+let filtered = features;
+
